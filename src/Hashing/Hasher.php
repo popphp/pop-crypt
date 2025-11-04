f202c580d525ea87122ed49d2156c75de399d663
Nick Sagona <nick@noladev.com>
2025-11-03 22:06:14 -0600

    Update version

--- src/Hashing/Hasher.php
+++ src/Hashing/Hasher.php
@@ -21,7 +21,7 @@ namespace Pop\Crypt\Hashing;
  * @author     Nick Sagona, III <dev@noladev.com>
  * @copyright  Copyright (c) 2009-2026 NOLA Interactive, LLC.
  * @license    https://www.popphp.org/license     New BSD License
- * @version    3.0.1
+ * @version    3.0.0
  */
 class Hasher
 {
